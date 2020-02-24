ffe834f60a982fa635f22f0c6d0c4ed4fad0dff6
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-02-25 00:39:46 +0100

    update

--- build.sh
+++ build.sh
@@ -40,6 +40,7 @@ for file in $docker_files; do
   docker buildx create ${BAREOS_APP}-${tag_build} --use
   docker buildx build \
     --platform "$build_arch" \
+    --output "type=local,dest=${BAREOS_APP}-${tag_build}.tar" \
     -t barcus/bareos-${BAREOS_APP}-new:${tag_build} ${app_dir}/${version_dir}
   #echo "${app_dir} ${release_dir} ${base_img} ${tag_build}" >> env/img_build
 done
