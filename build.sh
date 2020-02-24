1222a606c42cee085b8e868eadaffa7182303ca3
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-02-24 19:06:19 +0100

    update

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ branch=${CIRCLE_BRANCH}
 
 release=''
 re='^[0-9]+.*$'
-if [[ ${branch} =~ $re ]]; then
+if [ "${branch}" =~ $re ]; then
   release=$(echo ${branch} |sed 's#^\([0-9]*\).*$#\1#')
 fi
-if [[ -n ${tag} ]]; then
+if [ -n "${tag}" ]; then
   release=$(echo ${tag} |sed 's#^v\([0-9]*\)-.*$#\1#')
 fi
 
