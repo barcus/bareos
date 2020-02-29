2081d9adc923c2383f0d74f60936f72054478e67
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-01 00:25:15 +0100

    test github ci

--- prepare-build.sh
+++ prepare-build.sh
@@ -17,51 +17,51 @@
 # if $release is empty, build everything
 build_file='app_build.txt'
 rm $build_file
-docker_files=$(find ${BAREOS_APP}*/${release}* -name Dockerfile 2>/dev/null)
+docker_files=$(find . -name Dockerfile 2>/dev/null)
 
 for file in $docker_files; do
-  app_dir=$(echo $file |cut -d'/' -f1)
-  version_dir=$(echo $file |cut -d'/' -f2)
+  app=$(echo $file| sed -n 's#^\./\([a-z]*\).*#\1#p')
+  app_dir=$(echo $file |cut -d'/' -f2)
+  version_dir=$(echo $file |cut -d'/' -f3)
   version=$(echo $version_dir |cut -d'-' -f1)
   base_img=$(echo $version_dir |cut -d'-' -f2)
 
   # define default tag build
   tag_build="${version}-${base_img}"
-  if [ "${BAREOS_APP}" == 'director' ]; then
+  if [ "${app}" == 'director' ]; then
     backend=$(echo $app_dir |cut -d'-' -f2)
     tag_build="${tag_build}-${backend}"
   fi
 
   # create build file
   if [ "${base_img}" == 'ubuntu' ] ; then
-    echo "${BAREOS_APP} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
+    echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
 
     if [ "${backend}" != 'pgsql' ]; then
-      echo "${BAREOS_APP} ${version} amd64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} ${version} amd64 ${app_dir}/${version_dir}" >> $build_file
     fi
 
     if [ "${version}" == "$latest_ubuntu" ]; then
-      echo "${BAREOS_APP} ubuntu amd64 ${app_dir}/${version_dir}" >> $build_file
-      echo "${BAREOS_APP} latest amd64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} ubuntu amd64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} latest amd64 ${app_dir}/${version_dir}" >> $build_file
     fi
-    if [ "${BAREOS_APP}" == 'director' ]; then
-      echo "${BAREOS_APP} ${version}-ubuntu amd64 ${app_dir}/${version_dir}" >> $build_file
+    if [ "${app}" == 'director' ]; then
+      echo "${app} ${version}-ubuntu amd64 ${app_dir}/${version_dir}" >> $build_file
     fi
   fi
   if [ "${base_img}" == 'alpine' ]; then
-    echo "${BAREOS_APP} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
-    echo "${BAREOS_APP} ${tag_build} arm64 ${app_dir}/${version_dir}" >> $build_file
-    echo "${BAREOS_APP} ${tag_build} arm ${app_dir}/${version_dir}" >> $build_file
+    echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
+    echo "${app} ${tag_build} arm64 ${app_dir}/${version_dir}" >> $build_file
+    echo "${app} ${tag_build} arm ${app_dir}/${version_dir}" >> $build_file
 
-    if [ "${BAREOS_APP}" == "director" ]; then
-      echo "${BAREOS_APP} ${version}-alpine amd64 ${app_dir}/${version_dir}" >> $build_file
-      echo "${BAREOS_APP} ${version}-alpine arm64 ${app_dir}/${version_dir}" >> $build_file
-      echo "${BAREOS_APP} ${version}-alpine arm ${app_dir}/${version_dir}" >> $build_file
+    if [ "${app}" == "director" ]; then
+      echo "${app} ${version}-alpine amd64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} ${version}-alpine arm64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} ${version}-alpine arm ${app_dir}/${version_dir}" >> $build_file
     fi
     if [ "${version}" == "$latest_alpine" ]; then
-      echo "${BAREOS_APP} alpine amd64 ${app_dir}/${version_dir}" >> $build_file
-      echo "${BAREOS_APP} alpine arm64 ${app_dir}/${version_dir}" >> $build_file
-      echo "${BAREOS_APP} alpine arm ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} alpine amd64 ${app_dir}/${version_dir}" >> $build_file
+      echo "${app} alpine arm64 ${app_dir}/${version_dir}" >> $build_file
     fi
   fi
 done
