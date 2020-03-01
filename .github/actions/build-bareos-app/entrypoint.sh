4f6d637f9ceb128fd21baf759157476f9f63501f
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-01 12:10:40 +0100

    test github ci

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -17,9 +17,9 @@ docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --name builder --driver docker-container --use
 docker buildx inspect --bootstrap
 
-echo " app : $bareos_app"
+echo " app : $bareosApp"
 while read app version arch app_path ; do
-  if [ "$app" == "$bareos_app" ] ; then
+  if [ "$app" == "$bareosApp" ] ; then
     docker buildx build \
       --platform ${arch} \
       --output 'type=docker,push-false' \
