70e65e13b5482dc81c479f98b86a976e6579a8f2
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-01 02:46:19 +0100

    test github ci

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 export BUILDX_VER=v0.3.1
+apt update && apt install curl docker-ce
 mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
 curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
 chmod a+x ~/.docker/cli-plugins/docker-buildx
