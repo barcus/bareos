cbd1c57fdd6dbd57904ca6f0731b34092f9e8918
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-01 01:05:41 +0100

    test github ci

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-
+set -x
 while read app version arch app_path ; do
   docker buildx build \
     --platform ${arch} \
     --output 'type=docker,push-false' \
     --tag barcus/bareos-${app}:${version} \
     ${app_path}
-done < <(cat homework/app_build.txt |grep ${bareos_app})
+done < $(grep ${bareos_app} homework/app_build.txt)
