6c36c306b0a9c04f4aa8af1438bec13e16caf4dc
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-02 08:47:41 +0100

    test github ci

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
-export BUILDX_VER=v0.3.1
-export DOCKER_CLI_EXPERIMENTAL="enabled"
-
-# Install Buildx plugin
-mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
-curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
-chmod a+x ~/.docker/cli-plugins/docker-buildx
-
-# Run qemu
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#export BUILDX_VER=v0.3.1
+#export DOCKER_CLI_EXPERIMENTAL="enabled"
+#
+## Install Buildx plugin
+#mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
+#curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
+#chmod a+x ~/.docker/cli-plugins/docker-buildx
+#
+## Run qemu
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Create build and use it for building
 docker buildx create --name builder --driver docker-container --use
-workdir="${GITHUB_WORKSPACE}/build-artifact/"
+workdir="${GITHUB_WORKSPACE}/build"
 
 while read app version arch app_path ; do
   #if [ "$app" == "$INPUT_BAREOS_APP" ] ; then
+    env
     tag="${version}"
     re='^[0-9]+-alpine.*$'
     if [[ $version =~ $re ]] ; then
--- .github/actions/prepare-bareos-build/entrypoint.sh
+++ .github/actions/prepare-bareos-build/entrypoint.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+BUILDX_VER='v0.3.1'
 latest_ubuntu='19'
 latest_alpine='18'
-build_file="${GITHUB_WORKSPACE}/build/app_build.txt"
-docker_files=$(find . -name Dockerfile 2>/dev/null)
 
-echo $docker_files
+build_file="${GITHUB_WORKSPACE}/build/app_build.txt"
+build_app="$INPUT_BAREOS_APP"
+docker_files=$(find ${build_app}*/ -name Dockerfile 2>/dev/null)
 
 for file in $docker_files; do
-  app=$(echo $file| sed -n 's#^\./\([a-z]*\).*#\1#p')
-  app_dir=$(echo $file |cut -d'/' -f2)
-  version_dir=$(echo $file |cut -d'/' -f3)
+  app=$(echo $file| sed -n 's#^\([a-z]*\).*#\1#p')
+  app_dir=$(echo $file |cut -d'/' -f1)
+  version_dir=$(echo $file |cut -d'/' -f2)
   version=$(echo $version_dir |cut -d'-' -f1)
   base_img=$(echo $version_dir |cut -d'-' -f2)
 
@@ -22,11 +24,16 @@ for file in $docker_files; do
   fi
 
   # Declare each Dockerfile with its tags for building
-  if [ "${base_img}" == 'ubuntu' ]; then
-    echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
-  fi
+  echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
   if [ "${base_img}" == 'alpine' ]; then
-    echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
     echo "${app} ${tag_build} arm64 ${app_dir}/${version_dir}" >> $build_file
   fi
 done
+
+# Install Buildx plugin
+mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
+curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
+chmod a+x ~/.docker/cli-plugins/docker-buildx
+
+# Run qemu
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
