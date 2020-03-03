463fcdeab98bae171c166d72db795c821fa3d023
Marc Benslahdine <mbenslahdine@vente-privee.com>
2020-03-03 22:33:39 +0100

    test github ci

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -1,41 +1,27 @@
 #!/usr/bin/env bash
-#export BUILDX_VER=v0.3.1
-#export DOCKER_CLI_EXPERIMENTAL="enabled"
-#
-## Install Buildx plugin
-#mkdir -vp ~/.docker/cli-plugins/ ~/dockercache
-#curl --silent -L "https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64" > ~/.docker/cli-plugins/docker-buildx
-#chmod a+x ~/.docker/cli-plugins/docker-buildx
-#
-## Run qemu
-#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 # Create build and use it for building
 docker buildx create --name builder --driver docker-container --use
 workdir="${GITHUB_WORKSPACE}/build"
 
 while read app version arch app_path ; do
-  #if [ "$app" == "$INPUT_BAREOS_APP" ] ; then
-    #env
-    tag="${version}"
-    re='^[0-9]+-alpine.*$'
-    if [[ $version =~ $re ]] ; then
-      tag="${version}-${arch}"
-    fi
+  tag="${version}"
+  re='^[0-9]+-alpine.*$'
+  if [[ $version =~ $re ]] ; then
+    tag="${version}-${arch}"
+  fi
 
-    # Build with buildx
-    docker buildx build \
-      --platform ${arch} \
-      --output 'type=docker,push=false' \
-      --tag barcus/bareos-${app}:${tag} \
-      ${app_path}
+  # Build with buildx
+  docker buildx build \
+    --platform ${arch} \
+    --output 'type=docker,push=false' \
+    --tag barcus/bareos-${app}:${tag} \
+    ${app_path}
 
-    # Save image to file
-    docker save \
-      --output ${workdir}/bareos-${app}-${tag}.tar \
-      barcus/bareos-${app}:${tag}
-  #fi
+  # Save image to file
+  docker save \
+    --output ${workdir}/bareos-${app}-${tag}.tar \
+    barcus/bareos-${app}:${tag}
 done < ${workdir}/app_build.txt
 
 chmod 755 ${workdir}/bareos-*.tar
-ls -l  ${workdir}/
--- .github/actions/push-bareos-app/entrypoint.sh
+++ .github/actions/push-bareos-app/entrypoint.sh
@@ -10,33 +10,33 @@ done
 
 docker login -u barcus -p ${INPUT_DOCKER_PASS}
 
-# Push images
+# Push images and manifests
 while read app version arch app_path ; do
-  echo "on traite le tag build : $app $version $arch $app_path"
   build_tag=${version}
   re='^[0-9]+-alpine.*$'
   if [[ $version =~ $re ]] ; then
     build_tag="${version}-${arch}"
+    rm_tag="$rm_tag barcus/bareos-${app}:${build_tag}"
   fi
-
   # Push build_tag
   docker push barcus/bareos-${app}:${build_tag}
 done < ${workdir}/app_build.txt
 
 while read build_app s_tag t_tag ; do
-  echo "on traite le tag : $build_app $s_tag $t_tag"
+  # Add and push tag for Ubuntu 
   if [[ $s_tag =~ ^[0-9]+-ubuntu.*$ ]]; then
     docker tag barcus/bareos-${build_app}:${s_tag} barcus/bareos-${build_app}:${t_tag}
     docker push barcus/bareos-${build_app}:${t_tag}
   fi
-
+  # Create and push manifest for Alpuine (arm64 + amd64)
   if [[ $s_tag =~ ^[0-9]+-alpine.*$ ]]; then
-    echo "on cree la manifest : barcus/bareos-${build_app}:${t_tag} avec les tags : bareos-${build_app}:${s_tag}-amd64 et bareos-${build_app}:${s_tag}-arm64"
     docker manifest create barcus/bareos-${build_app}:${t_tag} \
       barcus/bareos-${build_app}:${s_tag}-amd64 \
       barcus/bareos-${build_app}:${s_tag}-arm64
-    docker manifest inspect barcus/bareos-${build_app}:${t_tag}
-    echo "on push le manifest barcus/bareos-${build_app}:${t_tag}"
     docker manifest push barcus/bareos-${build_app}:${t_tag}
   fi
 done < ${workdir}/tag_build.txt
+
+# Clean Alpine build_tag
+docker run --rm -it lumir/remove-dockerhub-tag --user barcus \
+  --password ${INPUT_DOCKER_PASS} $rm_tag
