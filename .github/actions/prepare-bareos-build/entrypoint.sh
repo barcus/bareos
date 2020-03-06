e79b70a3346bea0dd7be0e1a9c6a4ebbf05c7412
Barcus <barcus@tou.nu>
2020-03-07 00:24:01 +0100

    fix tags ubuntu

--- .github/actions/prepare-bareos-build/entrypoint.sh
+++ .github/actions/prepare-bareos-build/entrypoint.sh
@@ -29,13 +29,13 @@ for file in $docker_files; do
   if [ "${base_img}" == 'ubuntu' ]; then
     echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> $build_file
 
-    if [ "${app}" == 'director' ]; then
+    if [ "${app}" == 'director' ] && [ "${backend}" == 'mysql' ]; then
       echo "${app} ${tag_build} ${version}-ubuntu" >> $tag_file
     fi
     if [ "${backend}" != 'pgsql' ]; then
       echo "${app} ${tag_build} ${version}" >> $tag_file
     fi
-    if [ "${version}" == "$latest_ubuntu" ]; then
+    if [ "${version}" == "$latest_ubuntu" ] && [ "${backend}" != 'pgsql' ]; then
       echo "${app} ${tag_build} ubuntu" >> $tag_file
       echo "${app} ${tag_build} latest" >> $tag_file
     fi
