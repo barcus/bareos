e0ec5e01da252ae91be1ebfc46aa87f606fab18c
Barcus <barcus@tou.nu>
2020-03-07 09:36:23 +0100

    fix CI readme

--- .github/actions/push-bareos-readme/entrypoint.sh
+++ .github/actions/push-bareos-readme/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
 set -x
-cat /github/workspace/README.md
 
-ls -l /github/workspace/
 # Update Docker Hub overview
 docker run -v $PWD:/workspace \
-  -e DOCKERHUB_USERNAME=barcus \
-  -e DOCKERHUB_PASSWORD=$INPUT_DOCKER_PASS \
+  -e DOCKERHUB_USERNAME='barcus' \
+  -e DOCKERHUB_PASSWORD="$INPUT_DOCKER_PASS" \
   -e DOCKERHUB_REPOSITORY="${GITHUB_REPOSITORY}-${INPUT_BAREOS_APP}" \
   -e README_FILEPATH='/workspace/README.md' \
   peterevans/dockerhub-description:2.1.0
