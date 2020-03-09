bdc11ef708d556ae54afcb479afa12630cfcd825
Marc BENSLAHDINE <mbenslahdine@vente-privee.com>
2020-03-09 01:42:37 +0100

    dockerfile lint and cosmetics

--- director-mysql/17-ubuntu/docker-entrypoint.sh
+++ director-mysql/17-ubuntu/docker-entrypoint.sh
@@ -1,42 +1,60 @@
 #!/usr/bin/env bash
 
-# MAINTAINER Barcus <barcus@tou.nu>
+github_bareos='raw.githubusercontent.com/bareos/bareos'
+webui_admin_conf='/master/webui/install/bareos/bareos-dir.d/profile/webui-admin.conf'
+admin_conf='/master/webui//install/bareos/bareos-dir.d/console/admin.conf.example'
 
-if [ ! -f /etc/bareos/bareos-config.control ]
-  then
-  tar xfvz /bareos-dir.tgz
+if [ ! -f /etc/bareos/bareos-config.control ]; then
+  tar xfvz /bareos-dir.tgz --backup=simple --suffix=.before-control
 
   # Download default admin profile config
-  if [ ! -f /etc/bareos/bareos-dir.d/profile/webui-admin.conf ]
-    then
-    curl --silent --insecure https://raw.githubusercontent.com/bareos/bareos/master/webui/install/bareos/bareos-dir.d/profile/webui-admin.conf --output /etc/bareos/bareos-dir.d/profile/webui-admin.conf
+  if [ ! -f /etc/bareos/bareos-dir.d/profile/webui-admin.conf ]; then
+    curl --silent --insecure "https://${github_bareos}/${webui_admin_conf}" \
+      --output /etc/bareos/bareos-dir.d/profile/webui-admin.conf
   fi
- 
-  # Download default webUI config
-  if [ ! -f /etc/bareos/bareos-dir.d/console/admin.conf ]
-    then
-    curl --silent --insecure https://raw.githubusercontent.com/bareos/bareos/master/webui/install/bareos/bareos-dir.d/console/admin.conf.example --output /etc/bareos/bareos-dir.d/console/admin.conf
+
+  # Download default webUI admin config
+  if [ ! -f /etc/bareos/bareos-dir.d/console/admin.conf ]; then
+    curl --silent --insecure "https://${github_bareos}/${admin_conf}" \
+      --output /etc/bareos/bareos-dir.d/console/admin.conf
   fi
 
   # Update bareos-director configs
   # Director / mycatalog & mail report
-  sed -i "s#dbuser =.*#dbuser = root#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#dbpassword =.*#dbpassword = \"${DB_PASSWORD}\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#dbname =.*#dbname = bareos\n  dbaddress = \"${DB_HOST}\"\n  dbport = \"${DB_PORT}\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#/usr/bin/bsmtp -h localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" /etc/bareos/bareos-dir.d/messages/Daemon.conf
-  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" /etc/bareos/bareos-dir.d/messages/Daemon.conf
-  sed -i "s#/usr/bin/bsmtp -h localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" /etc/bareos/bareos-dir.d/messages/Standard.conf
-  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" /etc/bareos/bareos-dir.d/messages/Standard.conf
+  sed -i 's#dbuser =.*#dbuser = root#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i 's#dbpassword =.*#dbpassword = '\""${DB_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i 's#dbname =.*#dbname = bareos\n  dbaddress = '\""${DB_HOST}"\"'\n  dbport = '\""${DB_PORT}"\"'#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i "s#/usr/bin/bsmtp -h localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" \
+    /etc/bareos/bareos-dir.d/messages/Daemon.conf
+  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" \
+    /etc/bareos/bareos-dir.d/messages/Daemon.conf
+  sed -i "s#/usr/bin/bsmtp -h localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" \
+    /etc/bareos/bareos-dir.d/messages/Standard.conf
+  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" \
+    /etc/bareos/bareos-dir.d/messages/Standard.conf
+
   # storage daemon
-  sed -i "s#Address = .*#Address = \"${BAREOS_SD_HOST}\"#" /etc/bareos/bareos-dir.d/storage/File.conf
-  sed -i "s#Password = .*#Password = \"${BAREOS_SD_PASSWORD}\"#" /etc/bareos/bareos-dir.d/storage/File.conf
+  sed -i 's#Address = .*#Address = '\""${BAREOS_SD_HOST}"\"'#' \
+    /etc/bareos/bareos-dir.d/storage/File.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_SD_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/storage/File.conf
+
   # client/file daemon
-  sed -i "s#Address = .*#Address = \"${BAREOS_FD_HOST}\"#" /etc/bareos/bareos-dir.d/client/bareos-fd.conf
-  sed -i "s#Password = .*#Password = \"${BAREOS_FD_PASSWORD}\"#" /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+  sed -i 's#Address = .*#Address = '\""${BAREOS_FD_HOST}"\"'#' \
+    /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_FD_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+
   # webUI
-  sed -i "s#Password = .*#Password = \"${BAREOS_WEBUI_PASSWORD}\"#" /etc/bareos/bareos-dir.d/console/admin.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_WEBUI_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/console/admin.conf
+
   # MyCatalog Backup
-  sed -i "s#/var/lib/bareos/bareos.sql#/var/lib/bareos-director/bareos.sql#" /etc/bareos/bareos-dir.d/fileset/Catalog.conf
+  sed -i "s#/var/lib/bareos/bareos.sql#/var/lib/bareos-director/bareos.sql#" \
+    /etc/bareos/bareos-dir.d/fileset/Catalog.conf
 
   # Control file
   touch /etc/bareos/bareos-config.control
@@ -45,7 +63,7 @@ fi
 # MySQL check
 # Waiting for mysqld
 sqlup=1
-while [ "$sqlup" -ne 0 ] ; do 
+while [ "$sqlup" -ne 0 ] ; do
   echo "Waiting for mysqld..."
   mysqladmin --silent -u root -p"${DB_PASSWORD}" -h "${DB_HOST}" ping
   if [ $? -ne 0 ] ; then
@@ -74,6 +92,9 @@ if [ ! -f /etc/bareos/bareos-db.control ]
     /usr/lib/bareos/scripts/update_bareos_tables
 fi
 
+# Fix permissions
 find /etc/bareos/bareos-dir.d ! -user bareos -exec chown bareos {} \;
 chown bareos:bareos /var/lib/bareos
+
+# Run Dockerfile CMD
 exec "$@"
--- director-mysql/18-alpine/docker-entrypoint.sh
+++ director-mysql/18-alpine/docker-entrypoint.sh
@@ -1,46 +1,65 @@
 #!/usr/bin/env ash
 
-# MAINTAINER Barcus <barcus@tou.nu>
+github_bareos='raw.githubusercontent.com/bareos/bareos'
+webui_admin_conf='/master/webui/install/bareos/bareos-dir.d/profile/webui-admin.conf'
+admin_conf='/master/webui//install/bareos/bareos-dir.d/console/admin.conf.example'
 
-if [ ! -f /etc/bareos/bareos-config.control ]
-  then
-  tar xfvz /bareos-dir.tgz
+if [ ! -f /etc/bareos/bareos-config.control ]; then
+  tar xfvz /bareos-dir.tgz --backup=simple --suffix=.before-control
 
   # Download default admin profile config
-  if [ ! -f /etc/bareos/bareos-dir.d/profile/webui-admin.conf ]
-    then
-    curl --silent --insecure https://raw.githubusercontent.com/bareos/bareos/master/webui/install/bareos/bareos-dir.d/profile/webui-admin.conf --output /etc/bareos/bareos-dir.d/profile/webui-admin.conf
+  if [ ! -f /etc/bareos/bareos-dir.d/profile/webui-admin.conf ]; then
+    curl --silent --insecure "https://${github_bareos}/${webui_admin_conf}" \
+      --output /etc/bareos/bareos-dir.d/profile/webui-admin.conf
   fi
- 
-  # Download default webUI config
-  if [ ! -f /etc/bareos/bareos-dir.d/console/admin.conf ]
-    then
-    curl --silent --insecure https://raw.githubusercontent.com/bareos/bareos/master/webui/install/bareos/bareos-dir.d/console/admin.conf.example --output /etc/bareos/bareos-dir.d/console/admin.conf
+
+  # Download default webUI admin config
+  if [ ! -f /etc/bareos/bareos-dir.d/console/admin.conf ]; then
+    curl --silent --insecure "https://${github_bareos}/${admin_conf}" \
+      --output /etc/bareos/bareos-dir.d/console/admin.conf
   fi
 
   # Update bareos-director configs
   # Director / mycatalog & mail report
-  sed -i "s#dbuser =.*#dbuser = \"root\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#dbpassword =.*#dbpassword = \"${DB_PASSWORD}\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#dbname =.*#dbname = \"bareos\"\n  dbaddress = \"${DB_HOST}\"\n  dbport = \"${DB_PORT}\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#dbdriver = .*#dbdriver = \"mysql\"#" /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
-  sed -i "s#/usr/bin/bsmtp -h root@localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" /etc/bareos/bareos-dir.d/messages/Daemon.conf
-  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" /etc/bareos/bareos-dir.d/messages/Daemon.conf
-  sed -i "s#/usr/bin/bsmtp -h root@localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" /etc/bareos/bareos-dir.d/messages/Standard.conf
-  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" /etc/bareos/bareos-dir.d/messages/Standard.conf
+  sed -i 's#dbuser =.*#dbuser = root#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i 's#dbpassword =.*#dbpassword = '\""${DB_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i 's#dbname =.*#dbname = bareos\n  dbaddress = '\""${DB_HOST}"\"'\n  dbport = '\""${DB_PORT}"\"'#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i 's#dbdriver = .*#dbdriver = '\""mysql"\"'#' \
+    /etc/bareos/bareos-dir.d/catalog/MyCatalog.conf
+  sed -i "s#/usr/bin/bsmtp -h root@localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" \
+    /etc/bareos/bareos-dir.d/messages/Daemon.conf
+  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" \
+    /etc/bareos/bareos-dir.d/messages/Daemon.conf
+  sed -i "s#/usr/bin/bsmtp -h root@localhost#/usr/bin/bsmtp -h ${SMTP_HOST}#" \
+    /etc/bareos/bareos-dir.d/messages/Standard.conf
+  sed -i "s#mail = root@localhost#mail = ${ADMIN_MAIL}#" \
+    /etc/bareos/bareos-dir.d/messages/Standard.conf
+
   # storage daemon
-  sed -i "s#Address = .*#Address = \"${BAREOS_SD_HOST}\"#" /etc/bareos/bareos-dir.d/storage/File.conf
-  sed -i "s#Password = .*#Password = \"${BAREOS_SD_PASSWORD}\"#" /etc/bareos/bareos-dir.d/storage/File.conf
+  sed -i 's#Address = .*#Address = '\""${BAREOS_SD_HOST}"\"'#' \
+    /etc/bareos/bareos-dir.d/storage/File.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_SD_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/storage/File.conf
+
   # client/file daemon
-  sed -i "s#Address = .*#Address = \"${BAREOS_FD_HOST}\"#" /etc/bareos/bareos-dir.d/client/bareos-fd.conf
-  sed -i "s#Password = .*#Password = \"${BAREOS_FD_PASSWORD}\"#" /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+  sed -i 's#Address = .*#Address = '\""${BAREOS_FD_HOST}"\"'#' \
+    /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_FD_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/client/bareos-fd.conf
+
   # webUI
-  sed -i "s#Password = .*#Password = \"${BAREOS_WEBUI_PASSWORD}\"#" /etc/bareos/bareos-dir.d/console/admin.conf
+  sed -i 's#Password = .*#Password = '\""${BAREOS_WEBUI_PASSWORD}"\"'#' \
+    /etc/bareos/bareos-dir.d/console/admin.conf
+
   # MyCatalog Backup
   sql_opt="-u ${DB_USER} -p${DB_PASSWORD} -h ${DB_HOST} -P ${DB_PORT} -f --opt ${DB_NAME}"
-  echo -e "#!/bin/ash\n mysqldump ${sql_opt} > /var/lib/bareos/bareos.sql" > /etc/bareos/scripts/backup_catalog.sh
+  echo -e "#!/bin/ash\n mysqldump ${sql_opt} > /var/lib/bareos/bareos.sql" \
+    > /etc/bareos/scripts/backup_catalog.sh
   chmod +x /etc/bareos/scripts/backup_catalog.sh
-  sed -i "s#RunBeforeJob =.*#RunBeforeJob = \"/etc/bareos/scripts/backup_catalog.sh\"#" /etc/bareos/bareos-dir.d/job/BackupCatalog.conf
+  sed -i 's#RunBeforeJob =.*#RunBeforeJob = "/etc/bareos/scripts/backup_catalog.sh"#' /etc/bareos/bareos-dir.d/job/BackupCatalog.conf
   sed -i "s#/var/lib/bareos/bareos.sql#/var/lib/bareos-director/bareos.sql#" /etc/bareos/bareos-dir.d/fileset/Catalog.conf
 
   # Control file
@@ -50,7 +69,7 @@ fi
 # MySQL check
 # Waiting for mysqld
 sqlup=1
-while [ "$sqlup" -ne 0 ] ; do 
+while [ "$sqlup" -ne 0 ] ; do
   echo "Waiting for mysqld..."
   mysqladmin --silent -u root -p"${DB_PASSWORD}" -h "${DB_HOST}" ping
   if [ $? -ne 0 ] ; then
@@ -69,17 +88,19 @@ echo -e "[client]\nhost=${DB_HOST}\nuser=root\npassword=${DB_PASSWORD}" > /root/
 if [ ! -f /etc/bareos/bareos-db.control ]
   then
     # Init MySQL DB
-    /etc/bareos/scripts/create_bareos_database
-    /etc/bareos/scripts/make_bareos_tables
+    /usr/lib/bareos/scripts/create_bareos_database
+    /usr/lib/bareos/scripts/make_bareos_tables
 
     # Control file
     touch /etc/bareos/bareos-db.control
   else
     # Try MySQL DB upgrade
-    /etc/bareos/scripts/update_bareos_tables
+    /usr/lib/bareos/scripts/update_bareos_tables
 fi
 
-find /etc/bareos ! -user bareos -exec chown bareos {} \;
-chown bareos:bareos /var/lib/bareos
-chown bareos:bareos /var/log/bareos
+# Fix permissions
+find /etc/bareos/bareos-dir.d ! -user bareos -exec chown bareos {} \;
+chown bareos:bareos /var/lib/bareos /var/log/bareos
+
+# Run Dockerfile CMD
 exec "$@"
