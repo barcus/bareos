b7ad4de324e562bb112b80d60df6668080feed33
barcus <barcus@users.noreply.github.com>
2021-01-23 15:51:31 +0100

    Freeze alpine for Bareos v19 and add v20 (#93)

---
+++ client/20-alpine/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env ash
+
+bareos_fd_config="/etc/bareos/bareos-fd.d/director/bareos-dir.conf"
+
+if [ ! -f /etc/bareos/bareos-config.control ]; then
+  tar xzf /bareos-fd.tgz --backup=simple --suffix=.before-control
+
+  # Force client/file daemon password
+  sed -i 's#Password = .*#Password = '\""${BAREOS_FD_PASSWORD}"\"'#' $bareos_fd_config
+
+  # Control file
+  touch /etc/bareos/bareos-config.control
+fi
+
+# Fix permissions
+find /etc/bareos/bareos-fd.d ! -user bareos -exec chown bareos {} \;
+
+# Run Dockerfile CMD
+if [[ $FORCE_ROOT = true ]] ;then
+  export BAREOS_DAEMON_USER='root'
+fi
+exec "$@"
