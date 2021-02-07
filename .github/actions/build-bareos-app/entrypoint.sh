1bbef65d7abf4d681e6d3e7d6e3b3546d73922f7
Barcus <barcus@tou.nu>
2021-02-07 01:45:17 +0100

    improve actions

--- .github/actions/build-bareos-app/entrypoint.sh
+++ .github/actions/build-bareos-app/entrypoint.sh
@@ -9,7 +9,7 @@ cp "${workdir}/docker-buildx" ~/.docker/cli-plugins/
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 
 # Run Qemu
-docker run --rm --privileged tonistiigi/binfmt --install all
+#docker run --rm --privileged tonistiigi/binfmt --install all
 
 # Install git
 apk add --no-cache git
