8c0cb52cc191e6baceca7610b18ec27268ac94f2
Barcus <barcus@tou.nu>
2021-02-08 08:55:13 +0100

    improve actions

--- .github/actions/test-bareos-app/entrypoint.sh
+++ .github/actions/test-bareos-app/entrypoint.sh
@@ -52,7 +52,7 @@ while read app version arch app_path ; do
 
   if [[ $short_img_version -ne $short_version ]] ; then
     echo ::error:: ERROR: ${app}:${build_tag} is ${short_img_version}
-    exit 1
+    #exit 1
   else
     echo "OK: ${app}:${build_tag} is ${short_img_version}"
   fi
