eed3cbb4a16ecbba143de2a4c4a300bdb11a221c
barcus <barcus@users.noreply.github.com>
2021-02-16 09:32:03 +0100

    Improve Gitlab actions (#102)
    * improve Gitlab actions

--- .github/actions/prepare-bareos-app/entrypoint.sh
+++ .github/actions/prepare-bareos-app/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-BUILDX_VER='v0.3.1'
+BUILDX_VER='v0.5.1'
 latest_ubuntu='20'
 latest_alpine='20'
 
 build_file="${GITHUB_WORKSPACE}/build/app_build.txt"
 tag_file="${GITHUB_WORKSPACE}/build/tag_build.txt"
 build_app="$INPUT_BAREOS_APP"
-docker_files=$(find ${build_app}*/ -name Dockerfile 2>/dev/null)
+
+echo ::group::Get Dockerfile list
+docker_files=$(find "${build_app}"*/ -name Dockerfile |sort 2>/dev/null)
+echo "${docker_files}"
+echo ::endgroup::
 
 mkdir -p "${GITHUB_WORKSPACE}/build"
 
+echo ::group::'Build apps & tags list'
 for file in $docker_files; do
+  echo "processing $file"
   backend=''
   default_backend='mysql'
   app=$(echo "$file"| sed -n 's#^\([a-z]*\).*#\1#p')
@@ -30,8 +36,9 @@ for file in $docker_files; do
 
   # Declare each Dockerfile and tags related
   if [ "${base_img}" == 'ubuntu' ]; then
+    # Builds
     echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> "$build_file"
-
+    # Tags
     if [ "${app}" == 'director' ] && [ "${backend}" == "${default_backend}" ]; then
       echo "${app} ${tag_build} ${version}-ubuntu" >> "$tag_file"
       echo "${app} ${tag_build} ${version}" >> "$tag_file"
@@ -50,8 +57,10 @@ for file in $docker_files; do
   fi
 
   if [ "${base_img}" == 'alpine' ]; then
+    # Builds
     echo "${app} ${tag_build} amd64 ${app_dir}/${version_dir}" >> "$build_file"
     echo "${app} ${tag_build} arm64 ${app_dir}/${version_dir}" >> "$build_file"
+    # Tags
     echo "${app} ${tag_build} ${tag_build}" >> "$tag_file"
 
     if [ "${app}" == 'director' ] && [ "${backend}" == "${default_backend}" ]; then
@@ -65,6 +74,15 @@ for file in $docker_files; do
     fi
   fi
 done
+echo ::endgroup::
+
+# Debug output
+echo ::group::Debug output
+echo "### Build list"
+cat "$build_file"
+echo "### Tag list"
+cat "$tag_file"
+echo ::endgroup::
 
 # Download Docker Buildx plugin
 buildx_url="https://github.com/docker/buildx/releases/download/${BUILDX_VER}/buildx-${BUILDX_VER}.linux-amd64"
