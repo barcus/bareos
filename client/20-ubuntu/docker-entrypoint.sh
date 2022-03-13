6da23ca9ff7c77f9e56fd78dfe7f576e17e52921
barcus <barcus@users.noreply.github.com>
2022-03-13 14:04:52 +0100

    update bareos-fd (#138)
    * improve bareos-fd user settings

--- client/20-ubuntu/docker-entrypoint.sh
+++ client/20-ubuntu/docker-entrypoint.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/env bash
+#set -x
 
 bareos_fd_config="/etc/bareos/bareos-fd.d/director/bareos-dir.conf"
 
-if [ ! -f /etc/bareos/bareos-config.control ]; then
-  tar xzf /bareos-fd.tgz --backup=simple --suffix=.before-control
-
-  # Force client/file daemon password
-  sed -i 's#Password = .*#Password = '\""${BAREOS_FD_PASSWORD}"\"'#' $bareos_fd_config
-
-  # Control file
-  touch /etc/bareos/bareos-config.control
+if [ "${FORCE_ROOT}" = true ]; then
+  BAREOS_DAEMON_USER='root'
+  BAREOS_DAEMON_GROUP='root'
 fi
 
-# Fix permissions
-find /etc/bareos/bareos-fd.d ! -user bareos -exec chown bareos {} \;
+if [ $(id -u) = '0' ]; then
+  [ -n "${PUID}" ] && usermod -u ${PUID} ${BAREOS_DAEMON_USER}
+  [ -n "${PGID}" ] && groupmod -g ${PGID} ${BAREOS_DAEMON_GROUP}
 
-# Run Dockerfile CMD
-if [[ $FORCE_ROOT = true ]] ;then
-  export BAREOS_DAEMON_USER='root'
+  if [ ! -f /etc/bareos/bareos-config.control ]; then
+    tar xzf /bareos-fd.tgz --backup=simple --suffix=.before-control
+  
+    # Force client/file daemon password
+    sed -i 's#Password = .*#Password = '\""${BAREOS_FD_PASSWORD}"\"'#' $bareos_fd_config
+  
+    # Control file
+    touch /etc/bareos/bareos-config.control
+  fi
+  
+  # Fix permissions
+  find /etc/bareos ! -user ${BAREOS_DAEMON_USER} -exec chown ${BAREOS_DAEMON_USER}:${BAREOS_DAEMON_GROUP} {} \;
+  chown -R ${BAREOS_DAEMON_USER}:${BAREOS_DAEMON_GROUP} /var/lib/bareos /var/log/bareos
+
+  # Gosu
+  [ "${BAREOS_DAEMON_USER}" != 'root' ] && exec gosu "${BAREOS_DAEMON_USER}" "$BASH_SOURCE" "$@"
 fi
+
 exec "$@"
